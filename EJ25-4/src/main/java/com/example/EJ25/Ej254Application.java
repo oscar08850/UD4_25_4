7d8f85830e918fb50a4f97b13c37e583bd64637f
oscar08850 <oscar08850@gmail.com>
2022-11-17 17:21:13 +0100

    Esqueleto proyecto

---
+++ EJ25-4/src/main/java/com/example/EJ25/Ej254Application.java
@@ -0,0 +1,13 @@
+package com.example.EJ25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ej254Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ej254Application.class, args);
+	}
+
+}
---
+++ EJ25-4/src/test/java/com/example/EJ25/Ej254ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EJ25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ej254ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
